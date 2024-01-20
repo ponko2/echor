26f55f392aa82a30bd833ecc2c9c408c78d98d33
Takahito Nakano <kano@ponko2.jp>
2024-01-20 22:30:41 +0900

    Chapter 2.3.3

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,19 +1,43 @@
+use anyhow::Result;
 use assert_cmd::Command;
 use predicates::prelude::*;
 use std::fs;
 
 #[test]
-fn dies_no_args() {
-    let mut cmd = Command::cargo_bin("echor").unwrap();
-    cmd.assert()
+fn dies_no_args() -> Result<()> {
+    Command::cargo_bin("echor")?
+        .assert()
         .failure()
         .stderr(predicate::str::contains("Usage"));
+    Ok(())
+}
+
+fn run(args: &[&str], expected_file: &str) -> Result<()> {
+    let expected = fs::read_to_string(expected_file)?;
+    Command::cargo_bin("echor")?
+        .args(args)
+        .assert()
+        .success()
+        .stdout(expected);
+    Ok(())
 }
 
 #[test]
-fn hello1() {
-    let outfile = "tests/expected/hello1.txt";
-    let expected = fs::read_to_string(outfile).unwrap();
-    let mut cmd = Command::cargo_bin("echor").unwrap();
-    cmd.arg("Hello there").assert().success().stdout(expected);
+fn hello1() -> Result<()> {
+    run(&["Hello there"], "tests/expected/hello1.txt")
+}
+
+#[test]
+fn hello2() -> Result<()> {
+    run(&["Hello", "there"], "tests/expected/hello2.txt")
+}
+
+#[test]
+fn hello1_no_newline() -> Result<()> {
+    run(&["Hello  there", "-n"], "tests/expected/hello1.n.txt")
+}
+
+#[test]
+fn hello2_no_newline() -> Result<()> {
+    run(&["-n", "Hello", "there"], "tests/expected/hello2.n.txt")
 }
